1dd3055efeb9a9212d464b816c2c1d9a54349dee
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-28 10:34:08 +0400

    Added polygon graphic

--- src/main.js
+++ src/main.js
@@ -18,3 +18,16 @@ rectangle
   .drawRect(200, 200, 100, 120)
   .endFill()
 app.stage.addChild(rectangle);
+
+const poly = new PIXI.Graphics();
+poly
+  .beginFill(0xAA33BB)
+  .lineStyle(4, 0xFFEA00, 1)
+  .drawPolygon(
+      600, 50,
+      800, 150,
+      900, 300,
+      400, 400
+  )
+  .endFill()
+app.stage.addChild(poly);
