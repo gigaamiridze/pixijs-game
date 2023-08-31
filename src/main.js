fb0b440b2b55dd3bcd84a0a5078b0a5161ab44d9
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-31 13:30:45 +0400

    Added image and its parameters using Assets

--- src/main.js
+++ src/main.js
@@ -11,49 +11,63 @@ const app = new PIXI.Application({
 
 canvas.appendChild(app.view);
 
-const rectangle = new PIXI.Graphics();
-rectangle
-  .beginFill(0xAA33BB)
-  .lineStyle(4, 0xFFEA00, 1)
-  .drawRect(200, 200, 100, 120)
-  .endFill()
+const loadImage = async () => {
+  await PIXI.Assets.load('./images/drags.json');
+  const dragTexture = PIXI.Texture.from('drag11.png');
+  const dragSprite = new PIXI.Sprite(dragTexture);
 
-const poly = new PIXI.Graphics();
-poly
-  .beginFill(0xAA33BB)
-  .lineStyle(4, 0xFFEA00, 1)
-  .drawPolygon(
-      600, 50,
-      800, 150,
-      900, 300,
-      400, 400
-  )
-  .endFill()
+  dragSprite.anchor.set(0.5);
+  dragSprite.scale.set(1.3);
+  dragSprite.position.set(app.screen.width / 2, app.screen.height / 2);
 
-const circle = new PIXI.Graphics();
-circle
-  .beginFill(0xAA33BB)
-  .drawCircle(400, 200, 60)
-  .endFill()
+  app.stage.addChild(dragSprite);
+}
 
-const text = new PIXI.Text('Hello, World', {
-  fontFamily: 'Montserrat',
-  fontSize: 48,
-  fill: 'deepskyblue',
-  letterSpacing: 3,
-  stroke: '#ffffff',
-  strokeThickness: 4,
-  dropShadow: true,
-  dropShadowDistance: 10,
-  dropShadowAngle: Math.PI / 2,
-  dropShadowBlur: 4,
-  dropShadowColor: '#000000',
-});
+loadImage();
 
-app.stage.addChild(rectangle);
-app.stage.addChild(poly);
-app.stage.addChild(circle);
-app.stage.addChild(text);
+// const rectangle = new PIXI.Graphics();
+// rectangle
+//   .beginFill(0xAA33BB)
+//   .lineStyle(4, 0xFFEA00, 1)
+//   .drawRect(200, 200, 100, 120)
+//   .endFill()
+//
+// const poly = new PIXI.Graphics();
+// poly
+//   .beginFill(0xAA33BB)
+//   .lineStyle(4, 0xFFEA00, 1)
+//   .drawPolygon(
+//       600, 50,
+//       800, 150,
+//       900, 300,
+//       400, 400
+//   )
+//   .endFill()
+//
+// const circle = new PIXI.Graphics();
+// circle
+//   .beginFill(0xAA33BB)
+//   .drawCircle(400, 200, 60)
+//   .endFill()
+//
+// const text = new PIXI.Text('Hello, World', {
+//   fontFamily: 'Montserrat',
+//   fontSize: 48,
+//   fill: 'deepskyblue',
+//   letterSpacing: 3,
+//   stroke: '#ffffff',
+//   strokeThickness: 4,
+//   dropShadow: true,
+//   dropShadowDistance: 10,
+//   dropShadowAngle: Math.PI / 2,
+//   dropShadowBlur: 4,
+//   dropShadowColor: '#000000',
+// });
+
+// app.stage.addChild(rectangle);
+// app.stage.addChild(poly);
+// app.stage.addChild(circle);
+// app.stage.addChild(text);
 
 // app.ticker.add(delta => loop(delta));
 //
@@ -67,11 +81,11 @@ app.stage.addChild(text);
 //   app.stage.addChild(rect);
 // }
 
-const char1Sprite = PIXI.Sprite.from('./images/char1.png');
-char1Sprite.anchor.set(0.5, 0.5);
-char1Sprite.x = app.view.width / 2;
-char1Sprite.y = app.view.height / 2;
-app.stage.addChild(char1Sprite);
+// const char1Sprite = PIXI.Sprite.from('./images/char1.png');
+// char1Sprite.anchor.set(0.5, 0.5);
+// char1Sprite.x = app.view.width / 2;
+// char1Sprite.y = app.view.height / 2;
+// app.stage.addChild(char1Sprite);
 
 // app.ticker.add(delta => loop(delta));
 //
@@ -79,48 +93,48 @@ app.stage.addChild(char1Sprite);
 //   char1Sprite.rotation += 0.01;
 // }
 
-char1Sprite.eventMode = 'dynamic';
-char1Sprite.buttonMode = true;
-
-char1Sprite.on('pointerdown', () => {
-  char1Sprite.scale.x += 0.1;
-  char1Sprite.scale.y += 0.1;
-});
-
-document.addEventListener('keydown', (e) => {
-  switch (e.key) {
-    case 'ArrowRight':
-      return char1Sprite.x += 10;
-    case 'ArrowLeft':
-      return char1Sprite.x -= 10;
-    case 'ArrowUp':
-      return char1Sprite.y -= 10;
-    case 'ArrowDown':
-      return char1Sprite.y += 10;
-  }
-})
-
-const container = new PIXI.Container();
-
-const char2Sprite = PIXI.Sprite.from('./images/char2.png');
-char2Sprite.position.set(100, 100);
-
-const char3Sprite = PIXI.Sprite.from('./images/char3.png');
-char3Sprite.position.set(200, 200);
-
-const char4Sprite = PIXI.Sprite.from('./images/char4.png');
-char4Sprite.position.set(300, 300);
-
-container.addChild(char2Sprite);
-container.addChild(char3Sprite);
-
-app.stage.addChild(container);
-
-const init = async () => {
-  const texture = await PIXI.Assets.load('./images/char5.png');
-  const image = PIXI.Sprite.from(texture);
-  image.y = 300;
-  app.stage.addChild(image);
-}
-
-init();
+// char1Sprite.eventMode = 'dynamic';
+// char1Sprite.buttonMode = true;
+//
+// char1Sprite.on('pointerdown', () => {
+//   char1Sprite.scale.x += 0.1;
+//   char1Sprite.scale.y += 0.1;
+// });
+//
+// document.addEventListener('keydown', (e) => {
+//   switch (e.key) {
+//     case 'ArrowRight':
+//       return char1Sprite.x += 10;
+//     case 'ArrowLeft':
+//       return char1Sprite.x -= 10;
+//     case 'ArrowUp':
+//       return char1Sprite.y -= 10;
+//     case 'ArrowDown':
+//       return char1Sprite.y += 10;
+//   }
+// })
+//
+// const container = new PIXI.Container();
+//
+// const char2Sprite = PIXI.Sprite.from('./images/char2.png');
+// char2Sprite.position.set(100, 100);
+//
+// const char3Sprite = PIXI.Sprite.from('./images/char3.png');
+// char3Sprite.position.set(200, 200);
+//
+// const char4Sprite = PIXI.Sprite.from('./images/char4.png');
+// char4Sprite.position.set(300, 300);
+//
+// container.addChild(char2Sprite);
+// container.addChild(char3Sprite);
+//
+// app.stage.addChild(container);
+//
+// const init = async () => {
+//   const texture = await PIXI.Assets.load('./images/char5.png');
+//   const image = PIXI.Sprite.from(texture);
+//   image.y = 300;
+//   app.stage.addChild(image);
+// }
+//
+// init();
